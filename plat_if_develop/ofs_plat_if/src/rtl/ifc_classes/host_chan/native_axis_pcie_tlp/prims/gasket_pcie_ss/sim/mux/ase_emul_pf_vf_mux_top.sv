8771aa80514ca2e3ed83e48a29a5398b4976e22a
Michael Adler <Michael.Adler@intel.com>
2022-12-09 13:44:26 -0500

    Use SPDX-License-Identifier instead of long copyright headers

--- plat_if_develop/ofs_plat_if/src/rtl/ifc_classes/host_chan/native_axis_pcie_tlp/prims/gasket_pcie_ss/sim/mux/ase_emul_pf_vf_mux_top.sv
+++ plat_if_develop/ofs_plat_if/src/rtl/ifc_classes/host_chan/native_axis_pcie_tlp/prims/gasket_pcie_ss/sim/mux/ase_emul_pf_vf_mux_top.sv
@@ -1,36 +1,6 @@
-// ***************************************************************************
-//                               INTEL CONFIDENTIAL
-//
-//        Copyright (C) 2020 Intel Corporation All Rights Reserved.
-//
-// The source code contained or described herein and all  documents related to
-// the  source  code  ("Material")  are  owned  by  Intel  Corporation  or its
-// suppliers  or  licensors.    Title  to  the  Material  remains  with  Intel
-// Corporation or  its suppliers  and licensors.  The Material  contains trade
-// secrets  and  proprietary  and  confidential  information  of  Intel or its
-// suppliers and licensors.  The Material is protected  by worldwide copyright
-// and trade secret laws and treaty provisions. No part of the Material may be
-// used,   copied,   reproduced,   modified,   published,   uploaded,  posted,
-// transmitted,  distributed,  or  disclosed  in any way without Intel's prior
-// express written permission.
-//
-// No license under any patent,  copyright, trade secret or other intellectual
-// property  right  is  granted  to  or  conferred  upon  you by disclosure or
-// delivery  of  the  Materials, either expressly, by implication, inducement,
-// estoppel or otherwise.  Any license under such intellectual property rights
-// must be express and approved by Intel in writing.
-//
-// You will not, and will not allow any third party to modify, adapt, enhance, 
-// disassemble, decompile, reverse engineer, change or create derivative works 
-// from the Software except and only to the extent as specifically required by 
-// mandatory applicable laws or any applicable third party license terms 
-// accompanying the Software.
-//
-// -----------------------------------------------------------------------------
-// Create Date  : Nov 2020
-// Module Name  : pf_vf_demux.sv
-// Project      : IOFS
-// -----------------------------------------------------------------------------
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: MIT
+
 //
 // Description: 
 //
