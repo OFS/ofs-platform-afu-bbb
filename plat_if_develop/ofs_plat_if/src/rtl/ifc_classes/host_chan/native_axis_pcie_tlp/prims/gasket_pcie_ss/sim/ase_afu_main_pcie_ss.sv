4936ae870a9b97bd051138c7e4a5812f67eb9fb5
Michael Adler <Michael.Adler@intel.com>
2023-01-03 16:59:06 -0500

    Update ASE for shared afu_main() with a PF/VF MUX

--- plat_if_develop/ofs_plat_if/src/rtl/ifc_classes/host_chan/native_axis_pcie_tlp/prims/gasket_pcie_ss/sim/ase_afu_main_pcie_ss.sv
+++ plat_if_develop/ofs_plat_if/src/rtl/ifc_classes/host_chan/native_axis_pcie_tlp/prims/gasket_pcie_ss/sim/ase_afu_main_pcie_ss.sv
@@ -16,7 +16,12 @@ module ase_afu_main_pcie_ss
     input  logic uClk_usrDiv2
     );
 
+`ifdef AFU_MAIN_HAS_PF_VF_MUX
+    // afu_main() has a PF/VF MUX. Only generate one port into it.
+    localparam NUM_PORTS = 1;
+`else
     localparam NUM_PORTS = `OFS_PLAT_PARAM_HOST_CHAN_NUM_PORTS;
+`endif
 
     logic softReset;
     t_ofs_plat_power_state pwrState;
@@ -50,10 +55,17 @@ module ase_afu_main_pcie_ss
     // OFS sources specific to a given board.
     ase_afu_main_emul
       #(
-        .PG_NUM_PORTS(NUM_PORTS)
+        // The true number of ports, after demultiplexing
+        .PG_NUM_PORTS(`OFS_PLAT_PARAM_HOST_CHAN_NUM_PORTS)
         )
       ase_afu_main_emul
        (
+`ifdef AFU_MAIN_HAS_PF_VF_MUX
+        .afu_axi_tx_a_if(afu_axi_tx_a_if[0]),
+        .afu_axi_tx_b_if(afu_axi_tx_b_if[0]),
+        .afu_axi_rx_a_if(afu_axi_rx_a_if[0]),
+        .afu_axi_rx_b_if(afu_axi_rx_b_if[0]),
+`endif
         .*
         );
 
