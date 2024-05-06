8c74eca9add62af98c8fa09ae52c4da84a81d55f
Michael Adler <michael.adler@intel.com>
2024-05-06 15:19:11 -0400

    All AFUs can trigger interrupts
    Update host_chan_intr to instantiate on all ports.

--- plat_if_tests/host_chan_intr/hw/rtl/axi/ofs_plat_afu.sv
+++ plat_if_tests/host_chan_intr/hw/rtl/axi/ofs_plat_afu.sv
@@ -19,6 +19,8 @@ module ofs_plat_afu
     //
     // ====================================================================
 
+    localparam NUM_PORTS = plat_ifc.host_chan.NUM_PORTS;
+
     // Host memory AFU source
     ofs_plat_axi_mem_if
       #(
@@ -26,7 +28,7 @@ module ofs_plat_afu
         .BURST_CNT_WIDTH(7),
         .LOG_CLASS(ofs_plat_log_pkg::HOST_CHAN)
         )
-        host_mem_to_afu();
+        host_mem_to_afu[NUM_PORTS]();
 
     // 64 bit read/write MMIO AFU sink
     ofs_plat_axi_mem_lite_if
@@ -34,22 +36,28 @@ module ofs_plat_afu
         `HOST_CHAN_AXI_MMIO_PARAMS(64),
         .LOG_CLASS(ofs_plat_log_pkg::HOST_CHAN)
         )
-        mmio64_to_afu();
+        mmio64_to_afu[NUM_PORTS]();
 
     // Map FIU interface to AXI host memory and MMIO
-    ofs_plat_host_chan_as_axi_mem_with_mmio
-      #(
-        .ADD_TIMING_REG_STAGES(2)
-        )
-      primary_axi
-       (
-        .to_fiu(plat_ifc.host_chan.ports[0]),
-        .host_mem_to_afu,
-        .mmio_to_afu(mmio64_to_afu),
+    genvar p;
+    generate
+        for (p = 0; p < NUM_PORTS; p = p + 1)
+        begin : hc
+            ofs_plat_host_chan_as_axi_mem_with_mmio
+              #(
+                .ADD_TIMING_REG_STAGES(2)
+                )
+              primary_axi
+               (
+                .to_fiu(plat_ifc.host_chan.ports[p]),
+                .host_mem_to_afu(host_mem_to_afu[p]),
+                .mmio_to_afu(mmio64_to_afu[p]),
 
-        .afu_clk(),
-        .afu_reset_n()
-        );
+                .afu_clk(),
+                .afu_reset_n()
+                );
+        end
+    endgenerate
 
 
     // ====================================================================
@@ -60,11 +68,8 @@ module ofs_plat_afu
 
     ofs_plat_if_tie_off_unused
       #(
-        // Masks are bit masks, with bit 0 corresponding to port/bank zero.
-        // Set a bit in the mask when a port is IN USE by the design.
-        // This way, the AFU does not need to know about every available
-        // device. By default, devices are tied off.
-        .HOST_CHAN_IN_USE_MASK(1)
+        // All host channel ports are connected
+        .HOST_CHAN_IN_USE_MASK(-1)
         )
         tie_off(plat_ifc);
 
@@ -75,15 +80,20 @@ module ofs_plat_afu
     //
     // ====================================================================
 
-    afu
-     #(
-       .NUM_INTR_IDS(`OFS_PLAT_PARAM_HOST_CHAN_NUM_INTR_VECS)
-       )
-     afu
-      (
-       .host_mem_if(host_mem_to_afu),
-       .mmio64_if(mmio64_to_afu),
-       .pClk(plat_ifc.clocks.pClk.clk)
-       );
+    generate
+        for (p = 0; p < NUM_PORTS; p = p + 1)
+        begin : afu
+            afu
+              #(
+                .NUM_INTR_IDS(`OFS_PLAT_PARAM_HOST_CHAN_NUM_INTR_VECS)
+                )
+              afu
+               (
+                .host_mem_if(host_mem_to_afu[p]),
+                .mmio64_if(mmio64_to_afu[p]),
+                .pClk(plat_ifc.clocks.pClk.clk)
+                );
+        end
+    endgenerate
 
 endmodule // ofs_plat_afu
